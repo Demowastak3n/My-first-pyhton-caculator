4dbd27fb164c7b829bdd54948c1d77a0d9c0572b
Demoxn <demirozturk2011@gmail.com>
2026-01-08 22:55:50 +0300

    Update Learn Phyton 8.1.2026.py

--- Learn Phyton 8.1.2026.py
+++ Learn Phyton 8.1.2026.py
@@ -1,1 +1,26 @@
+unit = str(input("What is your unit (cm m km)"))
+number =float(input("What is your number"))
+choose = str(input("Which one do you want to transform cm m km"))
+
+if unit == "cm":
+    if choose == "cm":
+        print("Dont choose same units")
+    elif choose == "km":
+        print(number/100000)
+    elif choose == "m" :
+        print(number/100)
+elif unit == "km":
+    if choose == "km":
+        print("Dont choose same units")
+    elif choose == "m":
+        print(number*1000)
+    elif choose == "cm":
+        print(number*100000)
+elif unit == "m":
+    if choose == "m":
+        print("Dont choose same units")
+    elif choose == "cm":
+        print(number*100)
+    elif choose == "km":
+        print(number/1000)
 
