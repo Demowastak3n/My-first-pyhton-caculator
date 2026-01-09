782a3937bd1bea915dce9f376fa75b3b8b3d4cb4
Demoxn <demirozturk2011@gmail.com>
2026-01-09 20:29:29 +0300

    Update Learn Phyton 8.1.2026.py
    Added Length Caculations

--- Learn Phyton 8.1.2026.py
+++ Learn Phyton 8.1.2026.py
@@ -1,26 +1,59 @@
-unit = str(input("What is your unit (cm m km)"))
-number =float(input("What is your number"))
-choose = str(input("Which one do you want to transform cm m km"))
+import math
 
-if unit == "cm":
-    if choose == "cm":
-        print("Dont choose same units")
-    elif choose == "km":
-        print(number/100000)
-    elif choose == "m" :
-        print(number/100)
-elif unit == "km":
-    if choose == "km":
-        print("Dont choose same units")
-    elif choose == "m":
-        print(number*1000)
-    elif choose == "cm":
-        print(number*100000)
-elif unit == "m":
-    if choose == "m":
-        print("Dont choose same units")
-    elif choose == "cm":
-        print(number*100)
-    elif choose == "km":
-        print(number/1000)
+islem = str(input("What do you want to do (Calculator / Length): "))
+
+if islem == "Calculator":
+    num1 = float(input("Enter first number: "))
+    num2_input = input("Enter second number (press Enter to skip for sqrt): ")
+    
+    if num2_input == "":
+        # If no second number, automatically do sqrt
+        print(math.sqrt(num1))
+    else:
+        num2 = float(num2_input)
+        operator = str(input("Select an operation (+, -, *, /, %, ^, //): "))
+        
+        if operator == "+":
+            print(num1 + num2)
+        elif operator == "-":
+            print(num1 - num2)
+        elif operator == "*":
+            print(num1 * num2)
+        elif operator == "/":
+            print(num1 / num2)
+        elif operator == "%":
+            print(num1 % num2)
+        elif operator == "^":
+            print(num1 ** num2)
+        elif operator == "//":
+            print(num1 // num2)
+
+elif islem == "Length":
+    leg1 = float(input("Enter Length: "))
+    legOp1 = str(input("Current unit (km, cm, m): "))
+    legOp2 = str(input("Convert to (km, cm, m): "))
+
+    if legOp1 == "km":
+        if legOp2 == "cm":
+            print(leg1 * 100000)
+        elif legOp2 == "m":
+            print(leg1 * 1000)
+        else:
+            print("Invalid input")
+    elif legOp1 == "cm":
+        if legOp2 == "km":
+            print(leg1 / 100000)
+        elif legOp2 == "m":
+            print(leg1 / 100)
+        else:
+            print("Invalid input")
+    elif legOp1 == "m":
+        if legOp2 == "km":
+            print(leg1 / 1000)
+        elif legOp2 == "cm":
+            print(leg1 * 100)
+        else:
+            print("Invalid input")
+else:
+    print("Invalid input")
 
